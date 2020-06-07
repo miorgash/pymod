7495f52123d77b8ca1348b67cf04f0c524ef50de
miorgash <miorgash@gmail.com>
2020-06-07 05:52:09 +0000

    shusei

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ with open('LICENSE') as f:
 setup(
     name='pymod',
     version='0.1.0',
-    description='Python sample module'
+    description='Python sample module',
     long_description=readme,
     author='Miori Igarashi',
     author_email='miorgash@gmail.com',
